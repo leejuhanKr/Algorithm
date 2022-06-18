32eac48f20c75677b6e7e80b75f2a2a8e4d153e4
leejuhanKr <juhan96@gmail.com>
2022-06-18 21:59:18 +0900

    프로그래머스 테스트 풀이환경 구축

--- debug.py
+++ debug.py
@@ -1,3 +1,23 @@
 import os,sys
-a="python3 test.py<testcase.txt"
-os.system(a)
+# 파이썬 디버깅
+
+# TEST_CODE_PATH = "test.py"
+# TEST_CASE_PATH = "testcase.txt"
+
+# command = f'python3 {TEST_CODE_PATH} < {TEST_CASE_PATH}'
+
+# os.system(command)
+
+
+# 프로그래머스 디버깅
+commands = [
+    'echo "" > tmp_carrage.txt',
+    'cat testcase.txt tmp_carrage.txt test.py > tmp_test.py',
+    # 'python3 tmp_test.py'
+    # 'rm -rf tmp_test.py'
+]
+for command in commands:
+    os.system(command)
+
+
+
