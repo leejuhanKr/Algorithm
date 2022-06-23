ee4cd9c98a1219430d6c699e3bee91d2a91c7048
leejuhanKr <juhan96@gmail.com>
2022-06-23 22:05:17 +0900

    [level 2] Title: 교점에 별 만들기, Time: 0.02 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv2/87377. 교점에 별 만들기/교점에 별 만들기.py
+++ 프로그래머스/lv2/87377. 교점에 별 만들기/교점에 별 만들기.py
@@ -1,22 +1,17 @@
 def solution(line):
-
-    def get_cross_point(l1, l2):
-        a, b, e = l1
-        c, d, f = l2
-        if (divider := a*d-b*c) == 0:
-            return None
-        return ((b*f-e*d)/divider, (e*c-a*f)/divider)
-
-    x_lim = y_lim = [10e9,-10e9]
     x_lim = y_lim = [float('inf'),-float('inf')]
+    
     int_cross_point = []
     for i in range(len(line)):
         l1 = line[i]
+        
         for j in range(i+1,len(line)):
             l2 = line[j]
+            
             cross_point = get_cross_point(l1,l2)
             if cross_point == None:
                 continue
+                
             x,y = cross_point
             if all(map(float.is_integer,(x,y))):
                 x,y = int(x), int(y)
@@ -24,7 +19,10 @@ def solution(line):
                 x_lim = [min(x_lim[0],x), max(x_lim[1],x)]
                 y_lim = [min(y_lim[0],y), max(y_lim[1],y)]
 
-    cross_map = [['.']*(x_lim[1]-x_lim[0]+1) for _ in range(y_lim[1]-y_lim[0]+1)]
+    cross_map = [
+        ['.']*(x_lim[1]-x_lim[0]+1) 
+        for _ in range(y_lim[1]-y_lim[0]+1)
+    ]
 
     for x,y in int_cross_point:
         cross_map[y_lim[1]-y][x-x_lim[0]] = '*'
@@ -33,3 +31,10 @@ def solution(line):
         cross_map[i] = ''.join(v)
         
     return cross_map
+
+def get_cross_point(l1, l2):
+    a, b, e = l1
+    c, d, f = l2
+    if (divider := a*d-b*c) == 0:
+        return None
+    return ((b*f-e*d)/divider, (e*c-a*f)/divider)
