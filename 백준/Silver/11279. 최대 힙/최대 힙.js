fce9b648950956cb06650537eb82da8b7c944972
leejuhanKr <96106122+leejuhanKr@users.noreply.github.com>
2022-07-18 21:38:33 +0900

    [Silver II] Title: 최대 힙, Time: 252 ms, Memory: 24488 KB -BaekjoonHub

---
+++ 백준/Silver/11279. 최대 힙/최대 힙.js
@@ -0,0 +1,94 @@
+class MaxBinaryHeap {
+  constructor() {
+    this.values = [];
+  }
+  insert(element) {
+    this.values.push(element);
+    this.bubbleUp();
+  }
+  bubbleUp() {
+    let idx = this.values.length - 1;
+    const element = this.values[idx];
+    while (idx > 0) {
+      let parentIdx = Math.floor((idx - 1) / 2);
+      let parent = this.values[parentIdx];
+      if (element <= parent) break;
+      this.values[parentIdx] = element;
+      this.values[idx] = parent;
+      idx = parentIdx;
+    }
+  }
+  // remove
+  extractMax() {
+    const max = this.values[0];
+    const end = this.values.pop();
+    if (this.values.length > 0) {
+      this.values[0] = end;
+      this.sinkDown();
+    }
+    return max;
+  }
+  sinkDown() {
+    let idx = 0;
+    const length = this.values.length;
+    const element = this.values[0];
+    while (true) {
+      let leftChildIdx = 2 * idx + 1;
+      let rightChildIdx = 2 * idx + 2;
+      let leftChild, rightChild;
+      let swap = null;
+
+      if (leftChildIdx < length) {
+        leftChild = this.values[leftChildIdx];
+        if (leftChild > element) {
+          swap = leftChildIdx;
+        }
+      }
+      if (rightChildIdx < length) {
+        rightChild = this.values[rightChildIdx];
+        if (
+          (swap === null && rightChild > element) ||
+          (swap !== null && rightChild > leftChild)
+        ) {
+          swap = rightChildIdx;
+        }
+      }
+      if (swap === null) break;
+      this.values[idx] = this.values[swap];
+      this.values[swap] = element;
+      idx = swap
+    }
+  }
+  size(){
+    return this.values.length
+  }
+}
+
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "input.txt";
+let input = fs
+.readFileSync(filePath)
+.toString()
+.trim()
+.split("\n")
+.map((el) => Number(el));
+let n = Number(input[0]);
+
+
+let heap = new MaxBinaryHeap();
+
+let res = []
+
+input
+  .slice(1)
+  .forEach(el => {
+    if (el !== 0) {
+      heap.insert(el)
+      return
+    }
+    
+    if(!heap.size()) res.push(0)
+    else res.push(heap.extractMax())
+  })
+
+console.log(res.join('\n'))
