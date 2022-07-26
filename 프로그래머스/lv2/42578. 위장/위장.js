c7819b485d50df912a7c07c3f634a27174498af7
leejuhanKr <96106122+leejuhanKr@users.noreply.github.com>
2022-07-26 23:56:18 +0900

    [level 2] Title: 위장, Time: 0.30 ms, Memory: 30.2 MB -BaekjoonHub

--- 프로그래머스/lv2/42578. 위장/위장.js
+++ 프로그래머스/lv2/42578. 위장/위장.js
@@ -1,15 +1,7 @@
 function solution(clothes) {
-  const variations = {};
-  clothes.forEach(([_, type]) => {
-    if (typeof variations[type] === "undefined") {
-      variations[type] = 1;
-    } else {
-      variations[type]++;
-    }
-  });
-
-  return Object.values(variations)
-    .reduce((acc, cur) => acc * (cur + 1), 1) - 1;
+  const variations = clothes.reduce(
+    (acc, [_, cur]) => ({ ...acc, [cur]: (acc[cur] || 1) + 1 }),
+    {}
+  );
+  return Object.values(variations).reduce((acc, cur) => acc * cur, 1) - 1;
 }
-
-	
