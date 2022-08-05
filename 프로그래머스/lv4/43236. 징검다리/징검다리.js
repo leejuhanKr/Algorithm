5b63392cfe2fc2e542eff589be53653bcaf3838d
leejuhanKr <96106122+leejuhanKr@users.noreply.github.com>
2022-08-06 03:36:11 +0900

    [level 4] Title: 징검다리, Time: 0.14 ms, Memory: 29.8 MB -BaekjoonHub

--- 프로그래머스/lv4/43236. 징검다리/징검다리.js
+++ 프로그래머스/lv4/43236. 징검다리/징검다리.js
@@ -1,26 +1,35 @@
 function solution(distance, rocks, n) {
-  rocks = rocks.sort((a,b) => a-b)
-  rocks.push(distance)
-  return bisect_l(0,distance,cb,rocks,n)
+  rocks = rocks.sort((a, b) => a - b);
+  rocks.push(distance);
+  return  bisect_l(0, distance, check(rocks, n));
 }
-
-function cb(dists,n,maxDist) {
-  let pos = 0
-  for (let dist of dists) {
-    if (dist-pos<maxDist) {
-      n-=1
-      if (n<0) return false
+function bisect_l(l, r, cb) {
+  while (l <= r) {
+    mid = (l + r) >> 1;
+    if (cb(mid)) l = mid + 1;
+    else r = mid - 1;
+  }
+  return r;
+}
+function check(dists, n) {
+  return (maxDist) => {
+    let pos = 0;
+    let cnt = n;
+    for (let dist of dists) {
+      if (dist - pos < maxDist) {
+        cnt -= 1;
+        if (cnt < 0) return false;
+      } else pos = dist;
     }
-    else pos = dist
-  }
-  return true
+    return true;
+  };
 }
+/* 
+거리의 최솟값들의 최댓값이 target 이상이면 true를 반환하는 함수 cb
 
-function bisect_l(l,r,cb,dists,n) {
-  while (l<=r) {
-    mid = (l+r)>>1
-    if (cb(dists,n,mid)) {l = mid+1}
-    else {r = mid-1}
-  }
-  return r
-}
+target을 바위간 거리의 최솟값들 중 최댓값이라고 가정하고
+지워야할 바위들을 지워나가다 보면
+n을 초과하는 경우가 있다.
+이 경우는 최댓값이 target 미만 false를 리턴
+아닌 경우는 최댓값이 target 이상 true를 리턴
+*/
