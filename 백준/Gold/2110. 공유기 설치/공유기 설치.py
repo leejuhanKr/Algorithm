c8b90fd3a3a4cb77cfce8a520b15a5661ccf274c
leejuhanKr <96106122+leejuhanKr@users.noreply.github.com>
2022-08-06 01:28:29 +0900

    [Gold V] Title: 공유기 설치, Time: 336 ms, Memory: 52444 KB -BaekjoonHub

--- 백준/Gold/2110. 공유기 설치/공유기 설치.py
+++ 백준/Gold/2110. 공유기 설치/공유기 설치.py
@@ -1,33 +1,36 @@
 from sys import stdin
-N,C = map(int,input().split())
-X = list(map(int,stdin.readlines()))
 
-X.sort()
-d = [X[i]-X[i-1] for i in range(1,len(X))]
-# print(d)
-def check(d,c,t):
-    sum=0
-    i=0
-    c-=1
-    for i in d:
-        sum+=i
-        if sum >= t:
-            c-=1
-            sum=0
-        if not c:
-            return True
-    return False
+_, c = map(int,input().split())
+positions = sorted(map(int,stdin.readlines()))
+dists = [p2-p1 for p1,p2 in zip(positions,positions[1:])]
 
-# check(d,C,3)
+def check(dists, c):
+    """ 
+    각 집들간의 간격에 대한 배열 dists와 공유기의 개수 c를 받아
+    target이 주어졌을때 최댓값을 target보다 크거나 같게 만들수 있는지 반환하는 함수 리턴
+    """
+    def helper(target):
+        sum = 0
+        cnt = c-1 # 첫번째 설치후 남아있는 공유기 개수
+        for dist in dists:
+            sum+=dist
+            if sum>=target:
+                cnt-=1
+                if not cnt: # 더이상 설치할 공유기 없으면 true 반환
+                    return True
+                sum=0
+        return False
+    return helper
 
-l,r=0, X[-1]-X[0]
-while l<=r:
-    m = (l+r)//2
-    # print(l,m,r)
-    if check(d,C,m):
-        l=m+1
-    else:
-        r=m-1
-    # print(l,m,r)
+cb = check(dists, c)
 
-print(r)
+def bisect(l, r, cb):
+    while l <= r:
+        mid = (l+r)//2
+        if cb(mid):
+            l = mid + 1
+        else:
+            r = mid - 1
+    return r
+
+print(bisect(0,positions[-1]-positions[0],cb))
