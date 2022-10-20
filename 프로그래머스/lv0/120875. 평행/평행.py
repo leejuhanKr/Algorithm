d65ba22140324dfd33c99cf85f6cae43e9383ac6
Juhan Lee <96106122+leejuhanKr@users.noreply.github.com>
2022-10-21 00:18:03 +0900

    [level 0] Title: 평행, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv0/120875. 평행/평행.py
+++ 프로그래머스/lv0/120875. 평행/평행.py
@@ -1,8 +1,8 @@
 def solution(dots):
     combs = [(0,1,2,3), (0,2,1,3), (0,3,1,2)]
-    return +any([is_parallel([dots[i] for i in comb]) for comb in combs])
+    return +any([is_parallel(*(dots[i] for i in comb)) for comb in combs])
     
-def is_parallel(d):
-    return (d[1][0]-d[0][0])*(d[3][1]-d[2][1]) == (d[1][1]-d[0][1])*(d[3][0]-d[2][0])
+def is_parallel(d1,d2,d3,d4):
+    return (d2[0]-d1[0])*(d4[1]-d3[1]) == (d2[1]-d1[1])*(d4[0]-d3[0])
     
     
