8a91011424bb75f317db28cf3f68a2f4ac345163
Juhan Lee <96106122+leejuhanKr@users.noreply.github.com>
2022-11-04 00:56:12 +0900

    [unrated] Title: 우박수열 정적분, Time: 1.96 ms, Memory: 10.8 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/134239. 우박수열 정적분/우박수열 정적분.py
@@ -0,0 +1,18 @@
+def solution(k, ranges):
+    ks = []
+    while k != 1:
+        nk = k//2 if k%2==0 else k*3 + 1
+        ks.append(sum((k,nk))/2)
+        k=nk
+    return [
+        sum(ks[i:len(ks)+j]) if i<=len(ks)+j else -1
+        for i,j in ranges
+    ] 
+    
+    
+    
+    
+    
+    
+    
+    return []
