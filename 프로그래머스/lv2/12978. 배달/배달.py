40ccd0d6a53036915d0532e5d813fd72b24e1909
Juhan Lee <96106122+leejuhanKr@users.noreply.github.com>
2022-11-05 00:47:41 +0900

    [level 2] Title: 배달, Time: 15.73 ms, Memory: 10.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12978. 배달/배달.py
@@ -0,0 +1,30 @@
+from collections import namedtuple
+import heapq as hq
+
+Node = namedtuple('Node', ['weight', 'name'])
+
+def solution(N, road, K):
+    inf = 10e9
+    gh = [[inf]*(N+1) for _ in range(N+1)]
+    for s,e,w in road:
+        w = min(gh[s][e],w)
+        gh[s][e] = w
+        gh[e][s] = w
+        
+    dist = [inf]*(N+1)
+    dist[1] = 0
+    
+    q = []
+    node = Node(dist[1],1)
+    hq.heappush(q,node)
+    
+    while q:
+        node = hq.heappop(q)
+        for name, weight in enumerate(gh[node.name]):
+            if dist[name] < dist[node.name] + weight:
+                continue
+            dist[name] = dist[node.name] + weight
+            hq.heappush(q,Node(weight, name))
+
+    return sum(w<=K for w in dist)
+    
