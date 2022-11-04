7a346159e28eea3d59537f073966acbb56d10519
Juhan Lee <96106122+leejuhanKr@users.noreply.github.com>
2022-11-05 02:10:29 +0900

    [level 2] Title: [3차] 방금그곡, Time: 4.89 ms, Memory: 11.1 MB -BaekjoonHub

--- 프로그래머스/lv2/17683. ［3차］ 방금그곡/［3차］ 방금그곡.py
+++ 프로그래머스/lv2/17683. ［3차］ 방금그곡/［3차］ 방금그곡.py
@@ -9,17 +9,18 @@ def convert2time(time_string):
     return timedelta(**kargs)
 
 def solution(m, musicinfos):
-    m = ",".join(re.findall('C#|D#|F#|G#|A#|C|D|E|F|G|A|B', m))+','
+    patterns = 'C#|D#|F#|G#|A#|C|D|E|F|G|A|B'
+    m = ",".join(re.findall(patterns, m)) + ','
     infos = []
     for info in musicinfos:
         start,end,title,melody = info.split(',')
         runtime = int((convert2time(end) - convert2time(start)).total_seconds()//60)
-        melody = re.findall('C#|D#|F#|G#|A#|C|D|E|F|G|A|B', melody)
+        melody = re.findall(patterns, melody)
         infos.append(Music(runtime, title, melody))
     infos.sort(key=lambda x: -x.runtime)
     
     for runtime, title, melody in infos:
-        runtime_melody = ','.join(melody[i%len(melody)] for i in range(runtime))+','
+        runtime_melody = ','.join(melody[i%len(melody)] for i in range(runtime)) + ','
         if m in runtime_melody:
             return title
     return '(None)'
