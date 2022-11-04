71133dc147dc70c871df83cf3bf06f33bfe58ac1
Juhan Lee <96106122+leejuhanKr@users.noreply.github.com>
2022-11-05 01:00:12 +0900

    [level 2] Title: 배달, Time: 0.37 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv2/12978. 배달/배달.py
+++ 프로그래머스/lv2/12978. 배달/배달.py
@@ -2,28 +2,23 @@ from collections import namedtuple
 import heapq as hq
 
 Node = namedtuple('Node', ['weight', 'name'])
+inf = 10e9
 
 def solution(N, road, K):
-    inf = 10e9
     gh = [[inf]*(N+1) for _ in range(N+1)]
     for s,e,w in road:
-        w = min(gh[s][e],w)
-        gh[s][e] = gh[e][s] = w
+        gh[s][e] = gh[e][s] = min(gh[s][e],w)
         
     dist = [inf]*(N+1)
     dist[1] = 0
     
-    q = []
-    node = Node(dist[1],1)
-    hq.heappush(q,node)
-    
+    q = [Node(dist[1],1)]
     while q:
         node = hq.heappop(q)
         for name, weight in enumerate(gh[node.name]):
-            if dist[name] < dist[node.name] + weight:
-                continue
-            dist[name] = dist[node.name] + weight
-            hq.heappush(q,Node(weight, name))
+            if dist[name] > dist[node.name] + weight:
+                dist[name] = dist[node.name] + weight
+                hq.heappush(q,Node(dist[name], name))
 
     return sum(w<=K for w in dist)
     
