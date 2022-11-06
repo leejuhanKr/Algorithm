e2aef8a86c4d7d3ff22ad998f38d9416cdcd17f8
Juhan Lee <96106122+leejuhanKr@users.noreply.github.com>
2022-11-07 02:00:56 +0900

    [unrated] Title: 야간 전술보행, Time: 38.06 ms, Memory: 37.9 MB -BaekjoonHub

--- 프로그래머스/unrated/133501. 야간 전술보행/야간 전술보행.js
+++ 프로그래머스/unrated/133501. 야간 전술보행/야간 전술보행.js
@@ -1,14 +1,25 @@
 function solution(distance, scopes, times) {
-    scopes = scopes.map(scope => scope.sort((a,b)=> a-b))
-    console.log(scopes)
-    res = distance
-    scopes.forEach(([s,e],i) => {
+    // res = distance
+    // scopes.forEach((v,i) => {
+    //     const [w,r] = times[i]
+    //     const [s,e] = v.sort((a,b)=> a-b)
+    //     for (let d = s; d<=e; d++) {
+    //         if ((d-1)%(w+r) < w) {
+    //             res = Math.min(res,d)
+    //         }
+    //     }
+    // })
+    // return res
+    return scopes.reduce((acc,cur,i) => {
         const [w,r] = times[i]
+        const [s,e] = cur.sort((a,b)=> a-Math.min(acc,b))
+        // const _e = Math.min(e, acc)
+        console.log(s,e)
         for (let d = s; d<=e; d++) {
-            if (0<d%(w+r) &&  d%(w+r) <= w) {
-                res = Math.min(res,d)
+            if ((d-1)%(w+r) < w) {
+                return Math.min(acc,d)
             }
         }
-    })
-    return res
+        return acc
+    }, distance)
 }
