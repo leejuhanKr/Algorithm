d52e99fccc384a60048a20f4f1be9637ea1a083b
Juhan Lee <96106122+leejuhanKr@users.noreply.github.com>
2022-11-06 23:40:01 +0900

    [unrated] Title: 햄버거 만들기, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/133502. 햄버거 만들기/README.md
+++ 프로그래머스/unrated/133502. 햄버거 만들기/README.md
@@ -1,10 +1,10 @@
 # [unrated] 햄버거 만들기 - 133502 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/133502) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/133502?language=javascript) 
 
 ### 성능 요약
 
-메모리: 10.1 MB, 시간: 0.00 ms
+메모리: 33.4 MB, 시간: 0.06 ms
 
 ### 구분
 
---
+++ 프로그래머스/unrated/133502. 햄버거 만들기/햄버거 만들기.js
@@ -0,0 +1,11 @@
+function solution(ingredient) {
+    let stack = []
+    const burger = [1,2,3,1]
+    ingredient.forEach(v => {
+        stack.push(v)
+        if (burger.every((v,i) => stack.at(-4+i) === v)) {
+            burger.forEach(() => stack.pop())
+        }
+    })
+    return (ingredient.length-stack.length)/4
+}
