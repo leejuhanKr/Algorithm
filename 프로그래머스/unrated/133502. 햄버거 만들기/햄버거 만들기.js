226eabc17ceeee64c535e01d2e064bf4c19fdba4
Juhan Lee <96106122+leejuhanKr@users.noreply.github.com>
2022-11-07 14:02:59 +0900

    [unrated] Title: 햄버거 만들기, Time: 0.07 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/133502. 햄버거 만들기/햄버거 만들기.js
+++ 프로그래머스/unrated/133502. 햄버거 만들기/햄버거 만들기.js
@@ -3,9 +3,10 @@ function solution(ingredients) {
     const burger = [1,2,3,1]
     ingredients.forEach(ingredient => {
         stack.push(ingredient)
-        if (burger.every((b,i) => stack.at(-4+i) === b)) {
-        // if (burger.every((b,i) => stack.at(stack.length-4+i) === b)) {
-            burger.forEach(() => stack.pop())
+        // if (burger.every((b,i) => stack.at(-4+i) === b)) {
+        if (burger.every((b,i) => stack[stack.length-4+i] === b)) {
+            // burger.forEach(() => stack.pop())
+            stack.splice(-4, 4)
         }
     })
     return (ingredients.length-stack.length)/4
