72d113f2f470d1e3b9e95d5739b8eb84e324ec97
Juhan Lee <96106122+leejuhanKr@users.noreply.github.com>
2022-11-09 13:46:07 +0900

    [level 2] Title: 땅따먹기, Time: 45.59 ms, Memory: 73.4 MB -BaekjoonHub

--- 프로그래머스/lv2/12913. 땅따먹기/땅따먹기.js
+++ 프로그래머스/lv2/12913. 땅따먹기/땅따먹기.js
@@ -1,8 +1,14 @@
 function solution(land) {
-    for (let i = 1; i < land.length; i++) {
-        for (let j = 0; j < 4; j++) {
-            land[i][j] += Math.max(...land[i-1].filter((_,k) => j != k))
-        }
-    }
-    return Math.max(...land.at(-1))
+    const res = land.reduce((acc, cur, i) => {
+        return cur.map((v,i) => v + Math.max(...acc.filter((_, j) => j!=i)))
+    })
+    return Math.max(...res)
 }
+
+
+// for (let i = 1; i < land.length; i++) {
+//     for (let j = 0; j < 4; j++) {
+//         land[i][j] += Math.max(...land[i-1].filter((_,k) => j != k))
+//     }
+// }
+// return Math.max(...land.at(-1))
