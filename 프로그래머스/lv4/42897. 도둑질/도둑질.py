f6b3d531ed0baf70d92b5acd3182dbe3e951d45c
Juhan Lee <96106122+leejuhanKr@users.noreply.github.com>
2022-11-11 22:45:05 +0900

    [level 4] Title: 도둑질, Time: 400.17 ms, Memory: 43.3 MB -BaekjoonHub

--- 프로그래머스/lv4/42897. 도둑질/도둑질.py
+++ 프로그래머스/lv4/42897. 도둑질/도둑질.py
@@ -1,14 +1,22 @@
-def solution(money):
-    if len(money) <= 2:
-        return max(money, default=0)
-    
-    dp = [0, money[1], money[0], money[0]]
+def solution(m):
+    if len(m) <= 2:
+        return max(m, default=0)
+    dp = [0, m[1], m[0], m[0]]
+    for c in m[2:]:
+        dp = [max(dp[:2]), dp[0] + c, max(dp[2:]), dp[2] + c]
+    return max(dp[:3])
 
-    for m in money[2:]:
-        dp = [
-            max(dp[0:2]),
-            dp[0] + m,
-            max(dp[2:4]),
-            dp[2] + m,
-        ]
-    return max(dp[0:3])
+# def solution(money):
+#     if len(money) <= 2:
+#         return max(money, default=0)
+    
+#     dp = [0, money[1], money[0], money[0]]
+
+#     for m in money[2:]:
+#         dp = [
+#             max(dp[0:2]),
+#             dp[0] + m,
+#             max(dp[2:4]),
+#             dp[2] + m,
+#         ]
+#     return max(dp[0:3])
