dc0499acc45396719c8617c0eca89b5021760e39
Juhan Lee <96106122+leejuhanKr@users.noreply.github.com>
2022-11-11 18:28:36 +0900

    [level 3] Title: 스타 수열, Time: 0.04 ms, Memory: 9.97 MB -BaekjoonHub

--- 프로그래머스/lv3/70130. 스타 수열/스타 수열.py
+++ 프로그래머스/lv3/70130. 스타 수열/스타 수열.py
@@ -2,17 +2,18 @@ from collections import Counter
 
 def solution(a):
     counter = Counter(a)
-    prev = dict.fromkeys(counter, False)
+    prev = {k:[k, True] for k in counter}
+
     res = 0    
-    for i in prev:
-        if counter[i] > res:
+    for k in counter:
+        if counter[k] > res or 1:
             cnt = 0
             for v in a:
-                if type(prev[i]) is bool:
-                    prev[i]=v
-                elif (i == prev[i]) ^ (v==i):
+                if prev[1][k]:
+                    prev[k] = [v, False]
+                elif (k == prev[k][0]) ^ (v==k):
                     cnt+=1
-                    prev[i]= False
+                    prev[k][1] = True
             res = max(res, cnt)
-    
+
     return res*2
