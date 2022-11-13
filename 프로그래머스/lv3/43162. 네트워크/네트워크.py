95a65c25cfa61da99c8951b016f7279365c69f50
Juhan Lee <96106122+leejuhanKr@users.noreply.github.com>
2022-11-14 01:42:06 +0900

    [level 3] Title: 네트워크, Time: 0.29 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv3/43162. 네트워크/네트워크.py
+++ 프로그래머스/lv3/43162. 네트워크/네트워크.py
@@ -3,7 +3,7 @@ def solution(n, computers):
     _range = range(len(computers))
     sets = [{v} for v in _range]
     for i,j in product(_range, repeat=2):
-        if computers[i][j]:
+        if computers[i][j] and sets[i]!=sets[j]:
             sets[i]|=sets[j]
             for node in sets[j]:
                 sets[node] = sets[i]
