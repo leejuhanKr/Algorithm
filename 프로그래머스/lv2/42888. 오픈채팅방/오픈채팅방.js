f4205aa6f9164b084463024984b46bea1fb502b8
Juhan Lee <96106122+leejuhanKr@users.noreply.github.com>
2022-11-14 13:26:20 +0900

    [level 2] Title: 오픈채팅방, Time: 172.91 ms, Memory: 96.9 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42888. 오픈채팅방/오픈채팅방.js
@@ -0,0 +1,26 @@
+function solution(records) {
+  users = {}
+  logs = []
+  for (let record of records) {
+    const [state, id, username] = record.split(' ')
+    switch (state) {
+      case 'Enter':
+        if (!(id in users) || users[id] !== username) {
+          users[id] = username
+        }
+        logs.push([id, 1])
+        break
+      case 'Leave':
+        logs.push([id, 0])
+        break
+      case 'Change':
+        users[id] = username
+        break
+    }
+  }
+
+  return logs.map(
+    ([id, isEnterState]) =>
+      `${users[id]}님이 ${isEnterState ? '들어왔' : '나갔'}습니다.`,
+  )
+}
