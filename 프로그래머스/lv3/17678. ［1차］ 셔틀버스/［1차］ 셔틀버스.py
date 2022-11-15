ace6abb3f15cb0af78139deaca4d0c718ef441fc
Juhan Lee <96106122+leejuhanKr@users.noreply.github.com>
2022-11-15 14:35:29 +0900

    [level 3] Title: [1차] 셔틀버스, Time: 1.26 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv3/17678. ［1차］ 셔틀버스/［1차］ 셔틀버스.py
+++ 프로그래머스/lv3/17678. ［1차］ 셔틀버스/［1차］ 셔틀버스.py
@@ -1,13 +1,10 @@
-def solution(n, t, M, timetable):
-    res = "00:00"
+def solution(N, T, M, timetable):
+    res, bus = "00:00", "09:00"
     timetable.sort()
-    wait = 0
-    t_i = 0
-    bus = '09:00'
-    n_i = 0
+    wait, t_i, n_i = 0, 0, 0
     for h in range(24):
         for m in range(60):   
-            if n_i==n:
+            if n_i==N:
                 return res
             
             time = f'{h:0>2}:{m:0>2}'
@@ -15,13 +12,11 @@ def solution(n, t, M, timetable):
             while t_i<len(timetable) and timetable[t_i] <= time:
                 wait+=1
                 t_i+=1
-                flag =1
                 
-            if wait < M*(n-n_i):
+            if wait < M*(N-n_i):
                 res = time
                 
             if bus == time:
-                print(f'bus == {time}',wait -M)
                 wait -= M
                 if wait < 0:
                     res = time
@@ -29,11 +24,5 @@ def solution(n, t, M, timetable):
                 
             if bus <= time:
                 n_i += 1
-                _h,_m = divmod(t*n_i,60)
+                _h,_m = divmod(T*n_i,60)
                 bus = f'{9+_h:0>2}:{_m:0>2}'
-
-                
-
-                    
-                
-    
