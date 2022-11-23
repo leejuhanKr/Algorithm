a582515c16582c7400b192863bae27be7fda4e89
Juhan Lee <96106122+leejuhanKr@users.noreply.github.com>
2022-11-23 14:06:27 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.19 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv2/12981. 영어 끝말잇기/영어 끝말잇기.js
+++ 프로그래머스/lv2/12981. 영어 끝말잇기/영어 끝말잇기.js
@@ -1,15 +1,17 @@
 function solution(n, words) {
-    const used = new Set()
     let prev = words[0]
-    used.add(prev)
-    for (i = 1; i<words.length; i++) {
+    // const used = new Set([prev])
+    
+    // for (let [i,tmp] of Array.from(words.slice(1), (v,i) => [i+1,v])) {
+    for (let i = 1; i<words.length; i++) {
         tmp = words[i]
-        if (used.has(tmp) || prev.at(-1) !== tmp.at(0)) {
-            console.log(i)
+        // if (used.has(tmp) || prev.at(-1) !== tmp.at(0)) {
+        if (words.lastIndexOf(tmp,i-1)!==-1 || prev.at(-1) !== tmp.at(0)) {
             return [(i%n)+1, Math.floor(i/n)+1]
         }
-        used.add(tmp)
+        // used.add(tmp)
         prev = tmp
     }
+    
     return [0,0]
 }
