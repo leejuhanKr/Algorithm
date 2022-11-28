714c39099a9b29dc76ff6845a3222d99538124cd
Juhan Lee <96106122+leejuhanKr@users.noreply.github.com>
2022-11-28 13:43:02 +0900

    [level 2] Title: 괄호 변환, Time: 0.55 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/60058. 괄호 변환/괄호 변환.js
@@ -0,0 +1,32 @@
+function solution(p) {
+  if (p === '') return ''
+
+  const [u, v] = seperate(p)
+
+  return check(u)
+    ? `${u}${solution(v)}`
+    : '(' + solution(v) + ')' + reverse(u.slice(1, -1))
+}
+
+const seperate = p => {
+  idx = 0
+  sum = 0
+  do {
+    sum += { '(': 1, ')': -1 }[p[idx++]]
+  } while (sum !== 0)
+
+  return [p.slice(0, idx), p.slice(idx)]
+}
+
+const check = p => {
+  idx = 0
+  sum = 0
+  for (let c of p) {
+    sum += { '(': 1, ')': -1 }[p[idx++]]
+    if (sum < 0) return false
+  }
+  return true
+}
+
+const reverse = p =>
+  p.split('').reduce((acc, cur) => acc + { '(': ')', ')': '(' }[cur], '')
