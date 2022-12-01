57cb77d2c4919605e9d7fbdf8ff468a69774acd6
Juhan Lee <96106122+leejuhanKr@users.noreply.github.com>
2022-12-01 17:38:47 +0900

    [level 0] Title: 숨어있는 숫자의 덧셈 (2), Time: 0.02 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv0/120864. 숨어있는 숫자의 덧셈 （2）/숨어있는 숫자의 덧셈 （2）.py
+++ 프로그래머스/lv0/120864. 숨어있는 숫자의 덧셈 （2）/숨어있는 숫자의 덧셈 （2）.py
@@ -1,3 +1,19 @@
-import re
+# import re
+
+# def solution(my_string):
+#     return eval(re.sub('\D','+',my_string)+'+0')
+
 def solution(my_string):
-    return eval(re.sub('\D','+',my_string)+'+0')
+    return sum(i for i in gen(my_string))
+
+def gen(word):
+    res = ""
+    for w in word:
+        if w.isalpha():
+            if res:
+                yield int(res)
+                res=''
+        else:
+            res+=w
+    yield int(res) if res else 0
+            
