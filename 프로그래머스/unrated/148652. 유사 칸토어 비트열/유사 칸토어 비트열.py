a46473b5b3216238f2ff8e839927e7cd1ff066e0
Juhan Lee <96106122+leejuhanKr@users.noreply.github.com>
2023-01-01 22:03:29 +0900

    [unrated] Title: 유사 칸토어 비트열, Time: 1.24 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/148652. 유사 칸토어 비트열/유사 칸토어 비트열.py
@@ -0,0 +1,18 @@
+def solution(n, l, r):
+    return sum(i=='1' for i in foo(n, l-1, r))
+
+def foo(n,l,r):
+    if n == 0:
+        return '1'
+    ql, rl = divmod(l,5)
+    qr, rr = divmod(r,5)
+    
+    prev = foo(n-1, ql, qr + (1 if rr else 0))
+    res = ''
+    for char in prev:
+        if char == '1':
+            res += '11011'
+        else:
+            res += '00000'
+    return res[rl: rl+r-l]
+    
