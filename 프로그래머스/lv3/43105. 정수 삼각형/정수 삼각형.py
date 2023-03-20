6127c6f05b4ab69f1841b79c2895c9bbc1c7f6a6
Juhan Lee <96106122+leejuhanKr@users.noreply.github.com>
2023-03-21 03:14:51 +0900

    [level 3] Title: 정수 삼각형, Time: 93.38 ms, Memory: 40.8 MB -BaekjoonHub

--- 프로그래머스/lv3/43105. 정수 삼각형/정수 삼각형.py
+++ 프로그래머스/lv3/43105. 정수 삼각형/정수 삼각형.py
@@ -1,9 +1,23 @@
+# def solution(triangle):
+#     dp = triangle[0]
+#     for row in triangle[1:]:
+#         dp = [
+#             row[0]+dp[0],
+#             *(v+max(dp[i-1], dp[i]) for i, v in enumerate(row[1:-1],1)),
+#             row[-1]+dp[-1],
+#         ]
+#     return max(dp)
+from functools import lru_cache
+
+
 def solution(triangle):
-    dp = triangle[0]
-    for row in triangle[1:]:
-        dp = [
-            row[0]+dp[0],
-            *(v+max(dp[i-1], dp[i]) for i, v in enumerate(row[1:-1],1)),
-            row[-1]+dp[-1],
-        ]
-    return max(dp)
+    max_step = len(triangle) - 1
+
+    @lru_cache(maxsize=250000)
+    def clac(idx, step):
+        if step == max_step:
+            return triangle[max_step][idx]
+        return triangle[step][idx] + max(clac(idx, step + 1), clac(idx + 1, step + 1))
+
+    return clac(0, 0)
+    
