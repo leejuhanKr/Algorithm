6112069285efcaca6fcd6ef5e85acf08cf349a71
leejuhanKr <96106122+leejuhanKr@users.noreply.github.com>
2023-03-23 21:40:36 +0900

    변경사항

---
+++ 소프티어/비밀메뉴/solution.py
@@ -0,0 +1,26 @@
+from itertools import islice
+from sys import maxsize, stdin
+
+
+def preprocess_input():
+    M, N, K = map(int, stdin.readline().split())
+    key = tuple(int(i) for i in stdin.readline().split())
+    order = [int(i) for i in stdin.readline().split()]
+    return M, N, K, key, order
+
+
+def pairwise(iterable, _len):
+    yield from zip(*(islice(iterable, i, maxsize) for i in range(_len)))
+
+
+def solution():
+    M, N, K, secret_key, order = preprocess_input()
+    for key in pairwise(order, M):
+        print(key)
+        if secret_key == key:
+            print("gg")
+            return True
+    return False
+
+
+solution()
