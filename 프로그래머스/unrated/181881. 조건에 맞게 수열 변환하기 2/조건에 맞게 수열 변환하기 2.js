8e18ce672fb4a088f5ed81ba2c8e3261843737b2
Juhan Lee <96106122+leejuhanKr@users.noreply.github.com>
2023-05-29 21:41:44 +0900

    [unrated] Title: 조건에 맞게 수열 변환하기 2, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181881. 조건에 맞게 수열 변환하기 2/조건에 맞게 수열 변환하기 2.js
@@ -0,0 +1,17 @@
+function solution(arr) {
+    let i = 0
+    let flag
+    while (flag = true) {
+        arr = arr.map((v) => {
+            const newV = (v%2==0 && v>50) ? v/2 : (v%2==1 && v < 50) ? v*2 + 1 : v
+            if (flag && v!=newV) {
+                flag = false
+            }
+            return newV
+        })
+        if (flag) break;
+        i++
+        flag=true
+    }
+    return i
+}
