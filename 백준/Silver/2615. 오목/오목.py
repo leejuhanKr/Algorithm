5597b6181e2cda8af902759efdb540647af9aa11
Juhan Lee <96106122+leejuhanKr@users.noreply.github.com>
2023-09-03 15:37:01 +0900

    [Silver I] Title: 오목, Time: 144 ms, Memory: 38932 KB -BaekjoonHub

--- 백준/Silver/2615. 오목/오목.py
+++ 백준/Silver/2615. 오목/오목.py
@@ -1,41 +1,43 @@
 from sys import stdin
 from typing import Dict, Tuple
 
+Coordinate = Tuple[int, int]
 
-def solution(g: Dict[Tuple[int, int], int]):
-    arr = sorted(g)
 
-    for r, c in arr:
-        if block := g[r, c]:
+def solution(grid: Dict[Coordinate, int]) -> Tuple[int, Coordinate]:
+    sorted_coords = sorted(grid)
+
+    for r, c in sorted_coords:
+        if stone := grid[r, c]:
             for dx, dy in ((1, 0), (1, 1), (0, 1), (-1, 1)):
                 x, y = r - dx, c - dy
-                if g.get((x, y)) == block:
+                if grid.get((x, y)) == stone:
                     continue
 
                 x, y = r, c
                 for _ in range(5):
-                    if g.get((x, y)) != block:
+                    if grid.get((x, y)) != stone:
                         break
                     x += dx
                     y += dy
 
                 else:
-                    if g.get((x, y)) != block:
-                        return block, (r, c)
+                    if grid.get((x, y)) != stone:
+                        return stone, (r, c)
     return 0, (-1, -1)
 
 
 if __name__ == "__main__":
-    g = {}
+    grid = {}
     for r in range(19):
         row = map(int, stdin.readline().rstrip().split(" "))
 
         for c, val in enumerate(row):
             if val:
-                g[r, c] = val
+                grid[r, c] = val
 
-    block, pos = solution(g)
+    stone, pos = solution(grid)
 
-    print(block)
-    if block:
+    print(stone)
+    if stone:
         print(*map(lambda x: x + 1, pos))
